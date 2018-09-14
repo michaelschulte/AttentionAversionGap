04292a7b5766584e99f30adc9d8c6ab18b497e91
Schulte-Mecklenbeck, Michael <schulte@mpibp-berlin.mpg.de>
2018-09-14 16:42:16 +0200

    code checking
    added lottery problems

--- 1_process_data.R
+++ 1_process_data.R
@@ -1,5 +1,5 @@
 # Loss Vigilance
-# Michael Schulte-Mecklenbeck, Tomas Lejarraga
+# Tomas Lejarraga, Michael Schulte-Mecklenbeck
 # data cleaning
 
 # packages
@@ -10,7 +10,7 @@
   load('data/rawdata.Rdata')
 
 # remove participants based on less thank 90% choices made
-    test <- 
+  rawdata <- 
   rawdata %>%
     filter(!subject %in% c('c48', 'c61', 'm47', 'm66'))
 
@@ -24,21 +24,21 @@
     left_join(lotteryproblems, by = c('task', 'boxname'))
   
 # remove very short acquisitions <100ms on acquisition level
-  rawclean100  <- 
+  rawclean  <- 
   rawdata %>%
     filter(boxtime > 100)
   
 # remove outliers on boxtime level with mean > 2SD 
-  mean_boxtime <- mean(rawclean100$boxtime)
-  sd_boxtime <- sd(rawclean100$boxtime)
+  mean_boxtime <- mean(rawclean$boxtime)
+  sd_boxtime <- sd(rawclean$boxtime)
   
-  rawclean100 <- 
-  rawclean100 %>%
+  rawclean <- 
+  rawclean %>%
     filter(boxtime < mean_boxtime + 2*sd_boxtime) 
 
 # code actual values into 3 groups (small, middle, large)
-  rawclean100 <- 
-    rawclean100 %>%
+  rawclean <- 
+    rawclean %>%
     mutate(
       category = case_when(
         actual_value %in% c(67:100)   ~ "large",
@@ -50,17 +50,17 @@
     )
   
 # remove button events
-  rawclean100 <-  
-    rawclean100 %>%
+  rawclean <-  
+    rawclean %>%
     filter(!(type == 'bA' | type == 'bB')) # remove button events
   
 # convert submitted into real date 
-  rawclean100$time_corrected <- ymd_hms(rawclean100$submitted, locale = Sys.getlocale("LC_TIME"))
+  rawclean$time_corrected <- ymd_hms(rawclean$submitted, locale = Sys.getlocale("LC_TIME"))
   # order                                                     
-  rawclean100 <- arrange(rawclean100, time_corrected, counter)
+  rawclean <- arrange(rawclean, time_corrected, counter)
   # enumerate trial position into early, middle, late trials within each subjects
-  rawclean100 <- 
-  rawclean100 %>%
+  rawclean <- 
+  rawclean %>%
     group_by(trial, subject, time_corrected) %>%
     mutate(
       flag = case_when(
@@ -77,18 +77,19 @@
     )
 
 # re-order factor levels for gamble_position
-  rawclean100$position_gamble <- factor(rawclean100$position_gamble, levels = c("early", "middle", "late"))
+  rawclean$position_gamble <- factor(rawclean$position_gamble, levels = c("early", "middle", "late"))
   
 # change type labels O - Outcome / P - Probability
-  rawclean100 <- 
-    rawclean100 %>%
+  rawclean <- 
+    rawclean %>%
     mutate(type = as.factor(type)) %>%
     mutate(type = fct_recode(type,
                              "Outcome" = "O",
                              "Probability" = "P"))
 
 # add new column with cell based info pos / neg
-  rawclean100$value_pn <- str_sub(rawclean100$celltypeF, 2,4)
+  rawclean$value_pn <- str_sub(rawclean$celltypeF, 2,4)
   
 # save rawclean
-  save(rawclean100, file = 'data/rawclean100.Rdata')
+  save(rawclean, file = 'data/rawclean.Rdata') # 661483 rows | 27 variables
+  
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # LossVigilance
+
+This is a data and code repository for:
+
+Lejarraga, T., Schulte-Mecklenbeck, M., Pachur, T. & Hertwig, R. (2018). Loss Vigilance: A Cognitive Foundation of Loss Aversion. Evolution and Human Behavior. (submitted for publication).
+
+
